0eb40c4493edd0382e22ee45f55d630a47fa0a39
mrpremprakash <mr.premprakash@gmail.com>
2016-12-26 23:08:23 +0530

    Modified js files

---
+++ resources/views/invoice/add.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+export default class Invoice extends React.Component {
+  render() {
+    return (
+      <div className="page-content">
+        <div className="page-breadcrumbs">
+          <ul className="breadcrumb">
+            <li>
+                <i className="fa fa-folder-open"></i>
+                <a href="javascript:void(0);">Invoice</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="page-body">
+          <div className="row">
+            <div className="col-lg-12 col-sm-12 col-xs-12">
+                <div className="widget">
+                    <div className="widget-header bordered-bottom bordered-palegreen">
+                        <span className="widget-caption">Create New Invoice</span>
+                    </div>
+                    <div className="widget-body">
+                        <div>
+                            <form className="form-horizontal form-bordered" role="form" onSubmit={this.validate.bind(this)}>
+                                <div className="form-group">
+                                    <label for="inputEmail3" className="col-sm-2 control-label no-padding-right">Name:</label>
+                                    <div className="col-sm-10">
+                                        <input type="text" className="form-control" placeholder="Medicine Name" name="medicine_name"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label for="inputPassword3" className="col-sm-2 control-label no-padding-right">Quantity:</label>
+                                    <div className="col-sm-10">
+                                        <input type="text" className="form-control" placeholder="Quantity"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label for="inputPassword3" className="col-sm-2 control-label no-padding-right">Price:</label>
+                                    <div className="col-sm-10">
+                                        <input type="text" className="form-control" placeholder="Price"/>
+                                    </div>
+                                </div>
+
+                                <div className="form-group">
+                                    <label for="inputPassword3" className="col-sm-2 control-label no-padding-right">Discount:</label>
+                                    <div className="col-sm-10">
+                                        <input type="text" className="form-control" placeholder="Discount"/>
+                                    </div>
+                                </div>
+
+                                <div className="form-group">
+                                    <div className="col-sm-offset-2 col-sm-10">
+                                        <button type="submit" className="btn btn-palegreen">Add To Cart</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  };
+  validate(event) {
+    event.stopPropagation();
+    event.prevendDefault();
+    console.log(event);
+    return false;
+  }
+}
