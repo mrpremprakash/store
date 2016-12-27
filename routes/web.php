08f31942f43d009d14e466101f16b4b4485e77cd
mranupkmr <mr.anupkmr@gmail.com>
2016-12-27 21:10:30 +0530

    Add shop medicines

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::group(['middleware' => 'auth'], function()
 {
     Route::resource('shop_medicine', 'Shop_medicineController');
 });
-
+//Route::resource('shop_medicine', 'Shop_medicineController');
 
 Route::get('/invoice', function () {
     return view('pages.invoice');
